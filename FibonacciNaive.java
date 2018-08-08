cd52713dddfd146f94d34fc80c632d74fb5103b2
JING ZHAO <vickyhovey@gmail.com>
2018-08-07 20:06:45 -0400

    Update FibonacciNaive.java

--- FibonacciNaive.java
+++ FibonacciNaive.java
@@ -4,7 +4,7 @@ public class FibonacciNaive {
 
     public int fib(int n) {
         System.out.println("Start fib(" + n + ")");
-        if (n <=0 ) {
+        if (n <= 0 ) {
             return 0;
         } else if (n == 1) {
             return 1;
